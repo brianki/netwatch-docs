a2dfa11c01ec82dba245f772fd432bee3c2973b1
brianki <bkrotich4@gmail.com>
2026-05-07 20:09:11 +0300

    GLIP addition

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,18 +1,16 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import "@/public/custom.css";
+import Image from 'next/image';
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <Image
+          src="/images/iconnn.png"
+          alt="Netwatch.ai"
+          width={24}
+          height={24}
+        />
         Netwatch.ai
       </>
     ),
@@ -24,4 +22,4 @@ export const baseOptions: BaseLayoutProps = {
       active: 'nested-url',
     },
   ],
-};
+}
